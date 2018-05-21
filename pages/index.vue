30d90b976d31d169eca43da8a92a48b0bad8554a
katsuma narisawa <face.the.music721@gmail.com>
2018-05-21 23:20:22 +0900

    Initial Setup (#1)
    * Install stylus
    
    * Use syntax of stylus
    
    * Install pug
    
    * Use pug
    
    * Apply styles globally

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,16 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <nuxt-link
-          to="/about"
-          class="button--green">About</nuxt-link>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+html
+  body
+    section.container
+      div
+        app-logo
+        h1.title
+          | portfolio
+        h2.subtitle
+          | Nuxt.js project
+        .links
+          nuxt-link.button--green(to="/about") About
+          a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank") GitHub
 </template>
 
 <script>
@@ -81,33 +73,28 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="stylus" scoped>
+.container
+  min-height 100vh
+  display flex
+  justify-content center
+  align-items center
+  text-align center
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+.title
+  display block
+  font-weight 300
+  font-size 100px
+  color #35495e
+  letter-spacing 1px
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+.subtitle
+  font-weight 300
+  font-size 42px
+  color #526488
+  word-spacing 5px
+  padding-bottom 15px
 
-.links {
-  padding-top: 15px;
-}
+.links
+  padding-top 15px
 </style>
