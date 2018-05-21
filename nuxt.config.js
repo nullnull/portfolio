fd60bbd7371031ea670f7ed8c0fe9350881831a9
Katsuma Narisawa <face.the.music721@gmail.com>
2018-05-21 23:16:47 +0900

    Apply styles globally

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  css: [
+    '@/assets/css/main.styl'
+  ],
   /*
   ** Headers of the page
   */
--- pages/index.vue
+++ pages/index.vue
@@ -82,7 +82,6 @@ export default {
   text-align center
 
 .title
-  font-family "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif
   display block
   font-weight 300
   font-size 100px
