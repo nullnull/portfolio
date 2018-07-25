a68265b6480a96511d2e3700e4b26df19ea19535
Katsuma Narisawa <face.the.music721@gmail.com>
2018-07-25 19:20:13 +0900

    Base implementation

---
+++ components/Background.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  .root
+    transition(appear @enter='enter')
+      img(src='~/assets/images/me1.jpg')
+</template>
+
+<style lang="stylus" scoped>
+.root
+  display flex
+  justify-content space-around
+  flex-wrap: wrap
+  align-items center
+  height 100vh
+  width 100%
+  position absolute
+img
+  position absolute
+  left 0
+  right 0
+  margin auto
+  object-fit cover
+  opacity 0.5
+  height 100vh
+  width 100%
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,112 +1,76 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        About
-      </h1>
-      <h2 class="subtitle">
-        This is about page
-      </h2>
-      <div class="links">
-        <nuxt-link
-          to="/"
-          class="button--green">to TOP</nuxt-link>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+.root
+  Background
+  nuxt-link(to='/')
+    .container
+      .title.item
+        | about
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Background from '~/components/Background.vue'
 import {TweenMax, Elastic, Back} from 'gsap'
 
 export default {
   components: {
-    AppLogo
+    Background
   },
   transition: {
-    appear: false,
-    enter (el, done) {
+    appear: true,
+    async enter (el, done) {
+      await this.$delay(1000)
       requestAnimationFrame(() => {
-        TweenMax.staggerTo('.title', 1, {
+        TweenMax.staggerTo('.item', 3.3, {
           y: 0,
-          opacity: 1,
-          ease: Back.easeOut.config(3),
+          opacity: 0.9,
+          ease: 'ease',
           startAt: {
-            y: '40px',
+            y: 5,
             opacity: 0
           }
-        }, 0.1, () => {
-          done()
-        })
-        TweenMax.staggerTo('.container', 1, {
-          scaleX: 1,
-          x: 0,
-          opacity: 1,
-          transformOrigin: 'left center',
-          ease: Back.easeOut.config(3),
-          startAt: {
-            scaleX: 0,
-            x: '-20px',
-            opacity: 0
-          }
-        }, 0.1)
+        }, 1.3)
       })
     },
     leave (el, done) {
-      requestAnimationFrame(() => {
-        TweenMax.staggerTo('.container', 0.7, {
-          y: '-40px',
-          opacity: 0,
-          ease: Back.easeIn.config(3)
-        }, 0.1, () => {
-          done()
-        })
-        TweenMax.staggerTo('.container', 0.7, {
-          scaleX: 0,
-          x: '20px',
-          opacity: 0,
-          transformOrigin: 'right center',
-          ease: Back.easeIn.config(3)
-        }, 0.1)
-      })
+    //   requestAnimationFrame(() => {
+    //     TweenMax.staggerTo('.container', 0.7, {
+    //       y: '-40px',
+    //       opacity: 0,
+    //       ease: Back.easeIn.config(3)
+    //     }, 0.1, () => {
+    //       done()
+    //     })
+    //     TweenMax.staggerTo('.container', 0.7, {
+    //       scaleX: 0,
+    //       x: '20px',
+    //       opacity: 0,
+    //       transformOrigin: 'right center',
+    //       ease: Back.easeIn.config(3)
+    //     }, 0.1)
+    //   })
     }
   }
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="stylus" scoped>
+.container
+  min-height 100vh
+  display flex
+  justify-content center
+  align-items center
+  text-align center
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+.title
+  display block
+  text-transform uppercase
+  font-size 20px
+  letter-spacing 4px
+  display block
+  text-align center
+  position relative
+  font-weight 150
+  padding-bottom 15px
+  opacity 0
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
