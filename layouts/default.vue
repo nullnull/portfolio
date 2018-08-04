4e916cf3b31933dd87b06b1eb816322eca65336d
Katsuma Narisawa <face.the.music721@gmail.com>
2018-08-04 12:08:02 +0900

    Add loading animation (#15)
    * Implement image loader
    
    * Add loading animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,52 @@
 <template lang="pug">
   //- scroll handler(see mixins/handleScroll.js)
   div(@touchstart="startTouch($event)" @touchmove.prevent="onTouch($event)" @touchend="stopDrag($event)" @mousedown="startDrag($event)" @mousemove.prevent="onDrag($event)" @mouseup="stopDrag($event)" @wheel="onWheel($event)")
-    //- Show page
-    nuxt
-    //- Show next page background image for wipe animation
-    nextPageBackground
-    //- Show menu bar
-    Menu
+    div(v-show='!isLoading')
+      //- Show page
+      nuxt
+      //- Show next page background image for wipe animation
+      nextPageBackground
+      //- Show menu bar
+      Menu
+    transition(name='fade')
+      div(v-show='isLoading')
+        Loading
 </template>
 
 <script>
 import Menu from '~/components/Menu'
+import Loading from '~/components/Loading'
 import nextPageBackground from '~/components/layouts/default/NextPageBackground'
 import handleScroll from '~/mixins/handleScroll'
+import imageLoader from '~/mixins/imageLoader'
 
 export default {
+  computed: {
+    isLoading() {
+      return !this.$store.state.allImageLoaded;
+    }
+  },
   components: {
+    Loading,
     Menu,
     nextPageBackground,
   },
+  mounted() {
+    this.loadImage();
+  },
   mixins: [
-    handleScroll
+    handleScroll,
+    imageLoader,
   ],
 }
 </script>
+
+<style lang="stylus" scoped>
+.fade-enter-active,
+.fade-leave-active
+  transition: opacity 0.5s ease
+
+.fade-enter,
+.fade-leave-to
+  opacity: 0
+</style>
